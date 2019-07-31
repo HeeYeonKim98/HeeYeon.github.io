ca8a10f9a27c4c04df9ec4a2b48672a108e51239
HEEYEON KIM <wow9896@naver.com>
2019-07-31 19:52:15 +0900

    homework

---
+++ hiproject/acount/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['pw'] == request.POST['pw2']:
+            user = User.objects.create_user(username = request.POST['id'], password = request.POST['pw'])
+            auth.login(request, user)
+            return redirect('home')
+    return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['id']
+        password = request.POST['pw']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    return render(request, 'logout.html')
